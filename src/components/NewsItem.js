8f2a94775dccc2c60897b73e34f870c1d1d84378
BurhanuddinRaja <mails4burhan@gmail.com>
2022-06-09 19:33:07 +0530

    Articles displayed on website from sampleOutput

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -4,15 +4,15 @@ export class NewsItem extends Component {
     render() {
         // De-structuring
         // Using newsUrl becoz it is the only unique thing in the json data
-        let {title, description, imageUrl, newUrl} = this.props
+        let {title, description, imageUrl, newsUrl} = this.props
         return (
             <div>
                 <div className="card" style={{width: "18rem"}}>
-                    <img src={imageUrl} className="card-img-top" alt="..." />
+                    <img src={imageUrl} className="card-img-top" style={{height: "20vw", width: "100%", objectFit: "cover"}} alt="..." />
                         <div className="card-body">
-                            <h5 className="card-title">{title}</h5>
-                            <p className="card-text">{description}</p>
-                            <a href="/newsdetail" className="btn btn-primary">Go somewhere</a>
+                            <h5 className="card-title">{title}..</h5>
+                            <p className="card-text">{description}..</p>
+                            <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read in Detail</a>
                         </div>
                 </div>
             </div>
