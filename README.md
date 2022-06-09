089bce70c316a4bcd90fecff9373ef1efbcbe135
BurhanuddinRaja <mails4burhan@gmail.com>
2022-06-09 17:44:26 +0530

    State in class component

--- README.md
+++ README.md
@@ -4,3 +4,9 @@
 
 URL:- https://newsapi.org/ 
 I used the above link to get the API and Iused the data given in json format in my website NewsMonkey. 
+
+2. State in class based components
+
+- Here, you can use state by calling a constructor. In that constructor it is necessary to include `super()` because it will help display website or you will get an error.
+- `Props` are for read-only purpose but you can change the `State`.
+- You can initialize state by `this.state = { }` and set it by `this.setState`.
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,11 +3,12 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
     render() {
         // De-structuring
-        let {title, description} = this.props
+        // Using newsUrl becoz it is the only unique thing in the json data
+        let {title, description, imageUrl, newUrl} = this.props
         return (
             <div>
                 <div className="card" style={{width: "18rem"}}>
-                    <img src="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg" className="card-img-top" alt="..." />
+                    <img src={imageUrl} className="card-img-top" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">{title}</h5>
                             <p className="card-text">{description}</p>
